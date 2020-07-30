2e794380a5fcaeadf3a42cdd22376fdb9ce62545
Avadh <64136760+PatelAvadh@users.noreply.github.com>
2020-07-30 14:50:51 +0530

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -7,23 +7,28 @@ Created on Tue Jul 28 15:51:19 2020
 
 
 import numpy as np
-from Perceptron1 import Perceptron
+from perceptron import Perceptron
 
-
-training_inputs=[]
+training_inputs = []
 training_inputs.append(np.array([1,1]))
 training_inputs.append(np.array([1,0]))
 training_inputs.append(np.array([0,1]))
 training_inputs.append(np.array([0,0]))
 
-labels=np.array([1,0,0,0])
 
-Perceptron1 = Perceptron(2)
+labels = np.array([1,0,0,0])
 
-Perceptron1.train(training_inputs,labels)
+perceptron = Perceptron(2)
+perceptron.train(training_inputs,labels)
 
-inputs=np.array([1,1])
-print(Perceptron1.predict(inputs))
 
-inputs=np.array([0,0])
-print(Perceptron1.predict(inputs))
+inputs = np.array([1,1])
+print(perceptron.predict(inputs))
+
+print("========")
+
+inputs = np.array([0,1])
+print(perceptron.predict(inputs))
+
+
+print("final weights --> " + perceptron.weights)
