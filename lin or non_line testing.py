ce17784ef63e245da1a4a2227193713ced15c80c
Avadh <64136760+PatelAvadh@users.noreply.github.com>
2020-08-01 11:52:45 +0530

    Create lin or non_line testing.py

---
+++ lin or non_line testing.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 28 15:51:19 2020
+
+@author: Dell
+"""
+
+
+import numpy as np
+from Perceptron1 import Perceptron
+
+
+training_inputs=[]
+training_inputs.append(np.array([0,0,0,0]))
+training_inputs.append(np.array([0,0,0,1]))
+training_inputs.append(np.array([0,0,1,1]))
+training_inputs.append(np.array([0,1,0,0]))
+training_inputs.append(np.array([0,1,0,1]))
+training_inputs.append(np.array([0,1,1,0]))
+training_inputs.append(np.array([0,1,1,1]))
+training_inputs.append(np.array([1,0,0,0]))
+training_inputs.append(np.array([1,0,0,1]))
+training_inputs.append(np.array([1,0,1,0]))
+training_inputs.append(np.array([1,0,1,1]))
+training_inputs.append(np.array([1,1,0,0]))
+training_inputs.append(np.array([1,1,0,1]))
+training_inputs.append(np.array([1,1,1,0]))
+training_inputs.append(np.array([1,1,1,0]))
+training_inputs.append(np.array([1,1,1,1]))
+
+labels=np.array([0,0,0,0,0,0,0,0,0,1,1,1,0,1,0,1])
+Perceptron1 = Perceptron(4)
+Perceptron1.train(training_inputs,labels)
+
+inputs=np.array([0,0,0,0])
+print(Perceptron1.predict(inputs))
+
+inputs=np.array([1,1,1,1])
+print(Perceptron1.predict(inputs))
+
+inputs=np.array([1,0,1,0])
+print(Perceptron1.predict(inputs))
+
+inputs=np.array([0,1,0,1])
+print(Perceptron1.predict(inputs))
