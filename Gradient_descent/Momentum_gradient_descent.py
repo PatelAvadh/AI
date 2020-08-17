838df9d5e2b57cc6fc7839e7a1d54df981ece1d3
Avadh <64136760+PatelAvadh@users.noreply.github.com>
2020-08-17 15:29:57 +0530

    Update Momentum_gradient_descent.py

--- Gradient_descent/Momentum_gradient_descent.py
+++ Gradient_descent/Momentum_gradient_descent.py
@@ -1,74 +1,52 @@
-  
-import numpy as np
-import time
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 17 12:26:15 2020
 
-X=[0.5,2.5]
-Y=[0.2,0.9]
+@author: Dell
+"""
+import time
+import numpy as np
+X = [0.5,2.5]
+Y = [0.2,0.9]
 
 def f(w,b,x):
-    return 1.0/(1.0+np.exp(-(w*x+b)))
+    return 1/(1+np.exp(-(w*x+b)))
 
 def error(w,b):
-    err=0.0
+    err = 0.0
     for x,y in zip(X,Y):
         fx = f(w,b,x)
-        err+=0.5*(fx-y)**2
-    return err
-
-def grad_b(w,b,x,y):
-    fx=f(w,b,x)
-    return (fx-y)*(fx)*(1-fx)
+        err = err + 0.5*(fx-y)**2
+        return err
 
 def grad_w(w,b,x,y):
-    fx=f(w,b,x)
-    return (fx-y)*(fx)*(1-fx)*x
+    fx = f(w,b,x)
+    return (fx-y)*x*(1-fx)*fx
 
-def do_gradient_descent():
-    
-    tic = time.time()
-    w,b,eta,max_epoch = -7,-1,0.1,1000
-    
-    for i in range(max_epoch):
-        dw=0
-        db=0
-        for x,y in zip(X,Y):
-            dw+=grad_w(w, b, x, y)
-            db+=grad_b(w, b, x, y)
-            
-        w = w - eta*dw
-        b = b - eta*db
-    toc = time.time()
-    
-    print("Cost for vanila gradient descent : ",error(w, b))
-    print("Time required in seconds : ",toc - tic )
-    
+def grad_b(w,b,x,y):
+    fx = f(w,b,x)
+    return (fx-y)*(1-fx)*fx
+
 def do_momentum_gradient_descent():
-    
     tic = time.time()
-    w,b,eta,max_epoch = -7,-1,0.1,1000
-    gamma= 0.1
-    w_pre, b_pre = 0, 0
-    
+    w,b,eta,max_epoch = 0,0,1,10
+    gamma = 0.1
+    prev_v_w,prev_v_b = 0,0
     for i in range(max_epoch):
-        dw=0
-        db=0
+        dw,db = 0,0
+        for (x,y) in zip(X,Y):
+            dw += grad_w(w,b,x,y)
+            db += grad_b(w,b,x,y)
+        prev_v_w = w = w - ((gamma * prev_v_w) +(eta * dw))
+        prev_v_b = b - ((gamma * prev_v_b) +(eta * db))
         
-        for x,y in zip(X,Y):
-            dw+=grad_w(w, b, x, y)
-            db+=grad_b(w, b, x, y)
-            
-        w = ((gamma*w_pre) + (eta*dw))
-        b = ((gamma*b_pre) + (eta*db))
-        
-        w_pre=w
-        b_pre=b
-    
     toc = time.time()
     
-    print("Cost for momentum gradient descent : ",error(w, b))
-    print("Time required in seconds : ",toc - tic )
-    
-    
-do_gradient_descent()
-print("---------------------------------")
+    print("Final Weight :",w)
+    print("-------------")
+    print("Final Bias : ",b)
+    print("-------------")
+    print("Momentum Gradient Descent : ",error(w,b))
+    print("Time of execution is: ",toc-tic)
+
 do_momentum_gradient_descent()
