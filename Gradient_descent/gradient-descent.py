9b3c388398650bc7115d9f09988a5d5edd6e5f16
Avadh <64136760+PatelAvadh@users.noreply.github.com>
2020-08-17 15:30:30 +0530

    Update gradient-descent.py

--- Gradient_descent/gradient-descent.py
+++ Gradient_descent/gradient-descent.py
@@ -1,39 +1,52 @@
-#Problem : Identify the various effects on error while changing the values of bias, weights, learning rate and number of epochs
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 12 16:19:26 2020
 
+@author: Dell
+"""
+import time
 import numpy as np
-X =[0.5,2.5]
-Y =[0.2,0.9]
+X = [0.5,2.5]
+Y = [0.2,0.9]
 
 def f(w,b,x):
-  return 1.0/(1.0 + np.exp(-(w*x+b)))
+    return 1/(1+np.exp(-(w*x+b)))
 
 def error(w,b):
-  err= 0.0
-  for x,y in zip(X,Y):
-    fx = f(w,b,x)
-    err += 0.5 * (fx-y)**2
-  return err
-
-
-def grad_b(w,b,x,y):
-  fx = f(w,b,x)
-  return (fx-y)*(fx)*(1-fx)
+    err = 0.0
+    for x,y in zip(X,Y):
+        fx = f(w,b,x)
+        err = err + 0.5*(fx-y)**2
+        return err
 
 def grad_w(w,b,x,y):
-  fx = f(w,b,x)
-  return (fx-y)*(fx)*(1-fx)*x
+    fx = f(w,b,x)
+    return (fx-y)*x*(1-fx)*fx
+
+def grad_b(w,b,x,y):
+    fx = f(w,b,x)
+    return (fx-y)*(1-fx)*fx
 
 def do_gradient_descent():
-  w,b,eta,max_epoch = 1,1,10,2500
-  for i in range(max_epoch):
-    dw=0
-    db=0
-    for x,y in zip(X,Y):
-      dw += grad_w(w,b,x,y)
-      db += grad_b(w,b,x,y)
-    w = w - eta * dw
-    b = b - eta * db
-  print(error(w,b))  
-
-
+    tic = time.time()
+    w,b,lr,max_epoch = 0.9,0.01,17,7000
+    print(f"Weight {w}, Bias {b}, Learning_rate {lr}, Epoch {max_epoch}")
+    print("--------------")
+    for i in range(max_epoch):
+        dw =0
+        db =0
+        for x,y in zip(X,Y):
+            dw = dw + grad_w(w, b, x, y)
+            db = db + grad_b(w, b, x, y)
+        w = w - (lr*dw)
+        b = b - (lr*db)
+    toc = time.time()
+    print("Final Weight :",w)
+    print("-------------")
+    print("Final Bias : ",b)
+    print("-------------")
+    err = error(w,b)
+    print("Gradient descent Error",err)    
+    print("Time of execution is: ",toc-tic)
 do_gradient_descent()
+
